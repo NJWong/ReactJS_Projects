5b04a5c0fd65935691f01fdb01b7b8ae2f83dc00
Nick Wong <njwon4@gmail.com>
2016-06-03 12:56:33 +1000

    Added CommentForm, CommentList, and Comment react components.

--- FB_Tutorial/public/scripts/commentBox.js
+++ FB_Tutorial/public/scripts/commentBox.js
@@ -1,14 +1,52 @@
 var CommentBox = React.createClass({
-	render: function() {
-		return (
-			<div className="commentBox">
-				Hello, world! I am a CommentBox!
-			</div>
-		);
-	}
+    render: function() {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList />
+                <CommentForm />
+            </div>
+        );
+    }
+});
+
+var CommentList = React.createClass({
+    render: function() {
+        return (
+            <div className="commentList">
+                <Comment author="Nick Wong">This is Nicks comment</Comment>
+                <Comment author="Arvin Wiyono">This is Arvin comment</Comment>
+                <Comment author="Ray Cho">This is Ray comment</Comment>
+                <Comment author="Dash Yin">This is Dash comment</Comment>
+            </div>
+        );
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function() {
+        return (
+            <div className="commentForm">
+                <h1>CommentForm</h1>
+            </div>
+        );
+    }
+});
+
+var Comment = React.createClass({
+    render: function() {
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                {this.props.children}
+            </div>
+        );
+    }
 });
 
 ReactDOM.render(
-	<CommentBox />,
-	document.getElementById('content')
+    <CommentBox />,
+    document.getElementById('content')
 );
